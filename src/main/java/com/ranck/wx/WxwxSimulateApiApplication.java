1bbd9e1c1dfc8d29d48f3876e2b0c77c5c7df806
ranckprogram <1474574022@qq.com>
2019-08-08 22:11:02 +0800

    初始化项目 hello

---
+++ src/main/java/com/ranck/wx/WxwxSimulateApiApplication.java
@@ -0,0 +1,13 @@
+package com.ranck.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxwxSimulateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxwxSimulateApiApplication.class, args);
+    }
+
+}
