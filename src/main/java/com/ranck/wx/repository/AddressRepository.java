ca95644f1c685ba55c3ef744e16e0f3231c7cc98
ranckprogram <1474574022@qq.com>
2019-08-13 00:31:14 +0800

    Query与实体的对象一致性坑

--- src/main/java/com/ranck/wx/repository/AddressRepository.java
+++ src/main/java/com/ranck/wx/repository/AddressRepository.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 
-    @Query(nativeQuery = true, value = "SELECT * from address where my_id = :myId")
+//    @Query(nativeQuery = true, value = "SELECT * from address where my_id = :myId")
+//    List<Address> findList(@Param("myId") Integer myId);
+
+    // address 应该和实体的Entity name 属性值保持一致
+    @Query("SELECT o from address o where o.myId = :myId")
     List<Address> findList(@Param("myId") Integer myId);
 //
 //    @Query("DELETE FROM Address o where o.myId = :myId and o.friendId = :friendId")
